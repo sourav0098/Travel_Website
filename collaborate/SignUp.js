356bb79b8473362df18ce362557e0c20810c2ddf
MUSTAFA-U <n01414702@humbermail.ca>
2021-12-07 00:12:42 -0500

    changes

--- collaborate/SignUp.js
+++ collaborate/SignUp.js
@@ -14,7 +14,7 @@ function formValidation() {
     alert("Name length should be more than 2 and less than 21 charaters");
     userName.focus();
     return false;
-  }
+  }   
   // checking email format
   if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
     alert("Please enter a valid email!");
@@ -27,13 +27,7 @@ function formValidation() {
     password.focus();
     return false;
   }
-    // checking password character pattern
-  if (!password2.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/)) {
-    alert("Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character, and must be between 8 and 15 characters long.");
-    password2.focus();
-    return false;
-  }
-    // checking password character pattern
+    // checking reentered password is matching with the above entered password
   if (!password2.value.match(password.value)) {
     alert(" Reentered Password Does not match ");
     password2.focus();
