0e26316feed92cf3a7ddfa2f32a606792e55c356
MUSTAFA-U <n01414702@humbermail.ca>
2021-12-14 02:55:51 -0500

    changes

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -18,3 +18,39 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
+
+  // Select all input elements for varification
+const userName = document.getElementById("name");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("contact");
+const message = document.getElementById("message");
+
+// function for form varification
+function formValidation() {
+  // checking length of name
+  if (userName.value.length < 2 || userName.value.length > 20) {
+    document.getElementById("error").innerHTML = "Name length should be more than 2 and less than 21 charaters";
+    userName.focus();
+    return false;
+  }   
+  // checking email format
+  if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    document.getElementById("error").innerHTML = "Please enter a valid email!";
+    email.focus();
+    return false;
+  }
+  
+  // checking phone number
+  if (!phoneNumber.value.match(/^[1-9][0-9]{9}$/)) {
+    document.getElementById("error").innerHTML ="Phone number must be 10 characters long number and first digit can't be 0!";
+    phoneNumber.focus();
+    return false;
+  }
+  // checking gender
+  if (message.value === "") {
+    document.getElementById("error").innerHTML ="Please enter a message";
+    return false;
+  }
+  document.getElementById("error").innerHTML ="Message Sent";
+  return true;
+}
--- js/signUp.js
+++ js/signUp.js
@@ -11,45 +11,45 @@ const zipcode = document.getElementById("zipcode");
 function formValidation() {
   // checking length of name
   if (userName.value.length < 2 || userName.value.length > 20) {
-    alert("Name length should be more than 2 and less than 21 charaters");
+    document.getElementById("error").innerHTML = "Name length should be more than 2 and less than 21 charaters";
     userName.focus();
     return false;
   }   
   // checking email format
   if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-    alert("Please enter a valid email!");
+    document.getElementById("error").innerHTML = "Please enter a valid email!";
     email.focus();
     return false;
   }
   // checking password character pattern
   if (!password.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/)) {
-    alert("Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character, and must be between 8 and 15 characters long.");
+    document.getElementById("error").innerHTML = "Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character, and must be between 8 and 15 characters long.";
     password.focus();
     return false;
   }
     // checking reentered password is matching with the above entered password
-  if (!password2.value == password.value) {
-    alert(" Reentered Password Does not match ");
+  if (! password2.value == password.value) {
+    document.getElementById("error").innerHTML =" Reentered Password Does not match ";
     password2.focus();
     return false;
   }
   // checking phone number
   if (!phoneNumber.value.match(/^[1-9][0-9]{9}$/)) {
-    alert("Phone number must be 10 characters long number and first digit can't be 0!");
+    document.getElementById("error").innerHTML ="Phone number must be 10 characters long number and first digit can't be 0!";
     phoneNumber.focus();
     return false;
   }
   // checking gender
   if (gender.gender.value === "") {
-    alert("Please select your gender!");
+    document.getElementById("error").innerHTML ="Please select your gender!";
     return false;
   }
   // checking zip code
   if (!zipcode.value.match(/^[0-9]{6}$/) || zipcode.value=="") {
-    alert("Zip code must be 6 characters long number!");
+    document.getElementById("error").innerHTML ="Zip code must be 6 characters long number!";
     zipcode.focus();
     return false;
   }
-  alert("Registration Successful")
+  document.getElementById("error").innerHTML ="Registration Successful";
   return true;
 }
